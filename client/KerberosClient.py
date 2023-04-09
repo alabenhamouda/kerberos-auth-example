68e13aaa6b407b230512b66794a454443ee28dd3
alabh <alabenhmouda@gmail.com>
2023-04-09 20:44:59 +0100

    add upload file client

--- client/KerberosClient.py
+++ client/KerberosClient.py
@@ -3,9 +3,10 @@ import requests
 
 
 class KerberosClient:
-    class ClientHandler:
-        def __init__(self) -> None:
+    class Client:
+        def __init__(self, url) -> None:
             self.headers = {}
+            self.url = url
 
         def set_auth_header(self, negotiate_details):
             self.headers["Authorization"] = "Negotiate " + negotiate_details
@@ -13,13 +14,23 @@ class KerberosClient:
         def call(self) -> requests.Response:
             pass
 
-    class GetClientHandler(ClientHandler):
+    class GetClient(Client):
         def __init__(self, url):
-            super().__init__()
-            self.url = url
+            super().__init__(url)
 
         def call(self) -> requests.Response:
             return requests.get(self.url, headers=self.headers)
+
+    class UploadFileClient(Client):
+        def __init__(self, url, file_path):
+            super().__init__(url)
+            # Read the file to be uploaded
+            with open(file_path, 'rb') as file:
+                self.file_data = file.read()
+            self.headers['Content-Type'] = 'application/octet-stream'
+
+        def call(self) -> requests.Response:
+            return requests.post(self.url, data=self.file_data, headers=self.headers)
 
     def __init__(self, service):
         self.service = service
@@ -29,17 +40,17 @@ class KerberosClient:
         # auth_header is under the form negotiate {service_authenticator}
         return auth_header.split(" ")[1]
 
-    def kerberos_handshake(self, handler: ClientHandler):
+    def kerberos_handshake(self, client: Client):
         try:
             _, krb_context = kerberos.authGSSClientInit(self.service)
             kerberos.authGSSClientStep(krb_context, "")
             # grab the service ticket
             negotiate_details = kerberos.authGSSClientResponse(krb_context)
             # setup the auth header to use the kerberos ticket
-            handler.set_auth_header(negotiate_details=negotiate_details)
+            client.set_auth_header(negotiate_details=negotiate_details)
 
             # make the request
-            response = handler.call()
+            response = client.call()
 
             # authenticate the service
             service_auth = KerberosClient.get_service_authenticator_from_response(
@@ -53,5 +64,9 @@ class KerberosClient:
             return None
 
     def get(self, url):
-        handler = KerberosClient.GetClientHandler(url)
+        handler = KerberosClient.GetClient(url)
         return self.kerberos_handshake(handler)
+
+    def upload_file(self, url, file_path):
+        client = KerberosClient.UploadFileClient(url, file_path)
+        return self.kerberos_handshake(client)
