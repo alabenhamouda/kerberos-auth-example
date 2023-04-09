3b5537cd12bee0d28e7e28b8836c468be5078564
alabenhamouda <alabenhmouda@gmail.com>
2023-04-09 21:19:03 +0100

    add upload file route

--- server/server.py
+++ server/server.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from flask import Flask
+from flask import Flask, request
 from flask import render_template
 from flask_kerberos import init_kerberos
 from flask_kerberos import requires_authentication
@@ -19,6 +19,14 @@ def index(user):
 def test():
     return render_template('index.html', user="non authenticated")
 
+@app.route('/upload', methods=['POST'])
+@requires_authentication
+def upload_file(user):
+    print(user)
+    file = request.files['file']
+    print(file)
+    file.save('/home/kali/uploads/' + file.filename)
+    return 'File uploaded!', 200
 
 if __name__ == '__main__':
     init_kerberos(app, service="host")
