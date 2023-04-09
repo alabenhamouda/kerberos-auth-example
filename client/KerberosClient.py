bb01a61a70de079e3ffee5ded08b9f3a1edfc374
alabh <alabenhmouda@gmail.com>
2023-04-09 19:03:43 +0100

    refactor client to enhance code reusability

--- client/KerberosClient.py
+++ client/KerberosClient.py
@@ -3,6 +3,24 @@ import requests
 
 
 class KerberosClient:
+    class ClientHandler:
+        def __init__(self) -> None:
+            self.headers = {}
+
+        def set_auth_header(self, negotiate_details):
+            self.headers["Authorization"] = "Negotiate " + negotiate_details
+
+        def call(self) -> requests.Response:
+            pass
+
+    class GetClientHandler(ClientHandler):
+        def __init__(self, url):
+            super().__init__()
+            self.url = url
+
+        def call(self) -> requests.Response:
+            return requests.get(self.url, headers=self.headers)
+
     def __init__(self, service):
         self.service = service
 
@@ -11,17 +29,17 @@ class KerberosClient:
         # auth_header is under the form negotiate {service_authenticator}
         return auth_header.split(" ")[1]
 
-    def kerberos_handshake(self, url):
+    def kerberos_handshake(self, handler: ClientHandler):
         try:
             _, krb_context = kerberos.authGSSClientInit(self.service)
             kerberos.authGSSClientStep(krb_context, "")
             # grab the service ticket
             negotiate_details = kerberos.authGSSClientResponse(krb_context)
             # setup the auth header to use the kerberos ticket
-            headers = {"Authorization": "Negotiate " + negotiate_details}
+            handler.set_auth_header(negotiate_details=negotiate_details)
 
             # make the request
-            response = requests.get(url, headers=headers)
+            response = handler.call()
 
             # authenticate the service
             service_auth = KerberosClient.get_service_authenticator_from_response(
@@ -33,3 +51,7 @@ class KerberosClient:
         except Exception as err:
             print("Something is wrong with the ticket: " + err)
             return None
+
+    def get(self, url):
+        handler = KerberosClient.GetClientHandler(url)
+        return self.kerberos_handshake(handler)
--- client/client.py
+++ client/client.py
@@ -3,7 +3,7 @@ from KerberosClient import KerberosClient
 
 krb = KerberosClient("host@kdc.insat.tn")
 
-response = krb.kerberos_handshake("http://kdc.insat.tn:5000/")
+response = krb.get("http://kdc.insat.tn:5000/")
 
 if response is not None:
     print("status code: " + str(response.status_code))
